100fef0bd93f103fafbdaad761ff748de74bb7b3
Sven du Plessis <svenduplessis23@gmail.com>
2016-09-08 13:11:23 +0200

    added node class

--- LinkedListInterview/Program.cs
+++ LinkedListInterview/Program.cs
@@ -10,12 +10,50 @@ namespace LinkedListInterview
     {
         // your code goes here
         // tesing 123
+       
     }
 
     public class Program
     {
         public static void Main(string[] args)
         {
+        }
+    }
+
+    public class Node<T>
+    {
+        private T value;
+        private Node<T> nextNode;
+
+        public T Value
+        {
+            get
+            {
+                return value;
+            }
+
+            private set
+            {
+                this.value = value;
+            }
+        }
+
+        public Node<T> NextNode
+        {
+            get
+            {
+                return nextNode;
+            }
+
+            set
+            {
+                nextNode = value;
+            }
+        }
+
+        public Node(T value)
+        {
+            Value = value;
         }
     }
 }
