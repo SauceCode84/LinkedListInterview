8d627e3ba2d7ba1fe4da757010e4185b9fa58db7
Richard Ueckermann <rueckermann@sse.co.za>
2016-09-09 08:58:38 +0200

    added NUnit created first test - LinkedList_ImplementsGenericIEnumerable

---
+++ LinkedListInterview/LinkedListTests.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+using NUnit.Framework;
+
+namespace LinkedListInterview
+{
+    public class LinkedListTests
+    {
+        [Test]
+        public void LinkedList_ImplementsGenericIEnumerable()
+        {
+            LinkedList<int> list = new LinkedList<int>();
+
+            Assert.IsInstanceOf<IEnumerable<int>>(list);
+        }
+    }
+}
